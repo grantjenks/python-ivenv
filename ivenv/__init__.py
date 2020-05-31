48ada4dc191687015f3d703b2b502fc4ce61e347
Grant Jenks <grant.jenks@gmail.com>
2020-05-30 17:45:45 -0700

    Refactor activate/deactivate and add test

--- ivenv/__init__.py
+++ ivenv/__init__.py
@@ -9,56 +9,116 @@ import pathlib
 import site
 import sys
 
-BIN_NAME = 'bin'
+from typing import List, NamedTuple
+
+BIN_DIR = 'bin'
+LIB_DIR = 'lib'
 SITE_PACKAGES_GLOB = '*/site-packages'
 
 
-def activate(path):
-    """Activate virtual environment at `path`.
+class _State(NamedTuple):
+    """Virtual environment state.
 
     """
-    # TODO: Add heuristics/features for finding venv directory:
-    # - Expand home dir
-    # - Expand environment variables
-    # - Support path relative to file
-    venv_path = pathlib.Path(path)
-    # TODO: emit warning if Python versions do not match
-    venv_dir = venv_path.resolve(strict=True)
+    os_path: str
+    os_virtual_env: str
+    sys_path: List[str]
+    sys_prefix: str
+    sys_exec_prefix: str
 
-    # Update PATH environment variable.
+    @classmethod
+    def get(cls):
+        """Get state from `os` and `sys` settings.
 
-    path_dirs = os.environ.get("PATH", "").split(os.pathsep)
-    venv_bin_dir = venv_dir / BIN_NAME
-    path_dirs.insert(0, str(venv_bin_dir))
-    os.environ["PATH"] = os.pathsep.join(path_dirs)
+        """
+        return _State(
+            os_path=os.environ.get('PATH'),
+            os_virtual_env=os.environ.get('VIRTUAL_ENV'),
+            sys_path=sys.path.copy(),
+            sys_prefix=sys.prefix,
+            sys_exec_prefix=sys.exec_prefix,
+        )
 
-    # Update VIRTUAL_ENV environment variable.
+    def set(self):
+        """Set `os` and `sys` settings from state.
 
-    os.environ["VIRTUAL_ENV"] = str(venv_dir)
+        """
+        self._set_environ('PATH', self.os_path)
+        self._set_environ('VIRTUAL_ENV', self.os_virtual_env)
+        sys.path[:] = self.sys_path
+        sys.prefix = self.sys_prefix
+        sys.exec_prefix = self.sys_exec_prefix
 
-    # Record the length of the path to move the added paths from the end to the
-    # beginning.
-
-    path_len = len(sys.path)
-
-    # Find site packages paths.
-
-    site_paths = list((venv_dir / 'lib').glob(SITE_PACKAGES_GLOB))
-    for site_path in site_paths:
-        site.addsitedir(site_path)
-
-    # Move the sys paths added at the end back to the beginning.
-
-    sys.path[:] = sys.path[path_len:] + sys.path[:path_len]
-
-    # Update sys prefix attributes.
-
-    sys.real_prefix = sys.prefix
-    sys.prefix = str(venv_dir)
+    @staticmethod
+    def _set_environ(key, value):
+        if value is None:
+            os.environ.pop(key, None)
+        else:
+            os.environ[key] = value
 
 
-def deactivate():
-    pass
+class IVenv:
+    """Interactive Virtual Environments
+
+    """
+    def __init__(self):
+        start_state = _State.get()
+        self._stack: List[_State] = [start_state]
+
+    def activate(self, path):
+        """Activate virtual environment at `path`.
+
+        """
+        # IDEA: Add heuristics/features for finding venv directory:
+        # - Support path relative to file
+        vars_path = os.path.expandvars(path)
+        user_path = os.path.expanduser(vars_path)
+        venv_path = pathlib.Path(user_path)
+        venv_dir = venv_path.resolve(strict=True)
+        # IDEA: emit warning if Python versions do not match
+        state = _State(
+            os_path=self._make_os_path(venv_dir),
+            os_virtual_env=str(venv_dir),
+            sys_path=self._make_sys_path(venv_dir),
+            sys_prefix=str(venv_dir),
+            sys_exec_prefix=str(venv_dir),
+        )
+        state.set()
+        self._stack.append(state)
+
+    @staticmethod
+    def _make_os_path(venv_dir):
+        path_dirs = os.environ.get('PATH', "").split(os.pathsep)
+        venv_bin_dir = venv_dir / BIN_DIR
+        path_dirs.insert(0, str(venv_bin_dir))
+        os_path = os.pathsep.join(path_dirs)
+        return os_path
+
+    @staticmethod
+    def _make_sys_path(venv_dir):
+        path_len = len(sys.path)
+        site_paths = (venv_dir / LIB_DIR).glob(SITE_PACKAGES_GLOB)
+        for site_path in site_paths:
+            site.addsitedir(site_path)
+        sys_path = sys.path[path_len:] + sys.path[:path_len]
+        return sys_path
+
+    def deactivate(self):
+        """Deactivate virtual environment.
+
+        If no virtual environment is active, settings are restored to their
+        startup state.
+
+        """
+        if len(self._stack) > 1:
+            self._stack.pop()
+        state = self._stack[-1]
+        state.set()
+
+
+_ivenv = IVenv()
+activate = _ivenv.activate
+deactivate = _ivenv.deactivate
 
 
 __title__ = 'ivenv'
--- tests/test_ivenv.py
+++ tests/test_ivenv.py
@@ -2,8 +2,43 @@
 
 """
 
+import sys
+
+from pathlib import Path
+from subprocess import run
+from tempfile import TemporaryDirectory
+
+import pytest
+
 import ivenv
 
 
-def test_name():
-    assert ivenv.__title__ == 'ivenv'
+def test_ivenv():
+    # Make sure numpy is not installed.
+    with pytest.raises(ImportError):
+        import numpy
+
+    with TemporaryDirectory() as tempdir:
+        # Create virtual environment and install numpy.
+        venv_dir = Path(tempdir) / 'env'
+        python = ['python', '-I', '-m']
+        run([*python, 'venv', str(venv_dir)])
+        run([*python, 'pip', 'install', '--prefix', str(venv_dir), 'numpy'])
+
+        # Make sure numpy was installed into the new virtual environment.
+        with pytest.raises(ImportError):
+            import numpy
+
+        # Activate the new virtual environment and import numpy.
+        ivenv.activate(venv_dir)
+        import numpy
+        assert numpy.__version__
+
+        # "Un-import" numpy and deactivate the virtual environment.
+        del numpy
+        del sys.modules['numpy']
+        ivenv.deactivate()
+
+        # Make sure numpy cannot be imported now.
+        with pytest.raises(ImportError):
+            import numpy
